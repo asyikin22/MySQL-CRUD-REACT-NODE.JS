2411320126227a43f2a81eac0ba21fe06b24b472
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-05-24 23:27:32 +0800

    Add client folder with React application

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import Insert from "./pages/Insert";
+import Update from "./pages/Update";
+
+function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/insert" element={<Insert />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>  
+    </div>
+    
+
+  )
+  
+}
+
+export default App
+
---
+++ client/src/pages/Insert.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Insert = () => {
+  return (
+    <div>
+      <h1>Insert Page</h1>
+    </div>
+  )
+}
+
+export default Insert
